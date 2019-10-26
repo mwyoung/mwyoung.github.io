82f58b544796e8c183cff4c6c93e2a0eb99baa08
Miles Young <mwyoung2@gmail.com>
2019-10-26 16:12:15 -0700

    Added to webcrawlers

---
+++ ping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check if webpage exists, don't donwload
+ping_tool="wget --spider"
+site_map="https://mwyoung.github.io/sitemap.xml"
+
+declare -a se_array=("http://www.google.com/ping?sitemap=" \
+    "http://www.bing.com/webmaster/ping.aspx?siteMap=")
+
+for i in "${se_array[@]}"; do
+    echo $ping_tool $i$site_map
+    eval $ping_tool $i$site_map
+done
